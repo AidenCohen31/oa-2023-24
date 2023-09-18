24d5a7b0a5fe135383907367071c9d067c122872
Elon <elonmusk@gmail.com>
2023-09-18 16:03:25 -0500

    forgot to push

--- frontend/graph.mjs
+++ frontend/graph.mjs
@@ -1,8 +1,27 @@
 const graphDiv = document.getElementById("graph");
+const button = document.getElementById("but");
 
 fetch(
-    "https://oa-2023-24-backend.onrender.com" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
+    "localhost:5000/data?name="+ button.value //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
 ).then(async res => {
-    Plotly.newPlot( graphDiv, [ await res.json() ]); 
+    dx = []
+    dy = []
+    dt = []
+    json = res.json()
+    for(let i = 0; i < json.length; i ++){
+        dx.push(json[i][1])
+        dy.push(json[i][0])
+        dt.push(json[i][2])
+    }
+    Plotly.plot('graph',[{
+        mode:"text",
+        x: dx,
+        y: dy,
+        text: dt,
+        font: {
+            size: 1
+        }
+
+    }]); 
 })
 
